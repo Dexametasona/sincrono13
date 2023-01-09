8df149efbac6d04610f1f89091e8a07011896bac
Dexametasona <machicadogomezalexander@gmail.com>
2023-01-09 10:31:35 -0500

    tarea asincrona

--- src/app/comp-sinc13/asincrono/asincrono.component.ts
+++ src/app/comp-sinc13/asincrono/asincrono.component.ts
@@ -6,7 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asincrono.component.scss']
 })
 export class AsincronoComponent implements OnInit {
-
+  bd=[
+    {
+      "Apellido":"Arcilla",
+      "Casado":false,
+      "Dirección":"Calle 35 #43 28",
+      "Nombre":"Diego",
+      "Telefono":3859720
+    },
+    {
+      "Apellido":"Bueno",
+      "Casado":false,
+      "Dirección":"CR 16A #53 28",
+      "Nombre":"Kevin",
+      "Telefono":31485579954
+    },
+    {
+      "Apellido":"Palomino",
+      "Casado":false,
+      "Dirección":"CR 25 #52 33",
+      "Nombre":"Natalia",
+      "Telefono":32255945555
+    }
+    
+  ]
   constructor() { }
 
   ngOnInit(): void {
